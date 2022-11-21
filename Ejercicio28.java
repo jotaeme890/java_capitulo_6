7b965071b8b2878ae8441bc9e2a6471984932fd5
jotaeme890 <permanentjavi@gmail.com>
2022-11-21 08:46:11 +0100

    Ejercicio28

--- Ejercicio28.java
+++ Ejercicio28.java
@@ -12,48 +12,23 @@
  * @author jotaemee
  */
 
-import java.util.Scanner;
+
+
 public class Ejercicio28 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca el tamaño del array: ");
-        int n = sc.nextInt();
-        int[] a = new int[n];
+        int altura = (int)(Math.random()*6)*2 + 3; //Saco del 3 al 13 solo los impares
 
-        for (int i = 0; i < n; i++) {
-            a[i] = (int) (Math.random() * 201);
-            }
-            System.out.println("\nArray original:");
-            System.out.print("Índice ");
-            for (int i = 0; i < a.length; i++) {
-            System.out.printf("%5d", i);
-        }
-        System.out.print("\nValor ");
-
-        for (int i = 0; i < a.length; i++) {
-        
+        System.out.print("*    *  ****  *       **** \n");
+        for(int i = 1;i<altura/2;i++){
+            System.out.print("*    * *    * *      *    *\n");
         }
 
-        int[] r = new int[n];
-        int izquierda = 0;
-        int derecha = n - 1;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-            r[izquierda++] = a[i];
-            } else {
-            r[derecha--] = a[i];
-            }
+        System.out.print("****** *    * *      ******\n");
+        for(int i = 1;i<altura/2;i++){
+            System.out.print("*    * *    * *      *    *\n");
         }
-        System.out.println("\n\nArray resultado:");
-        System.out.print("Índice ");
-        for (int i = 0; i < r.length; i++) {
-            System.out.printf("%5d", i);
-        }
-        System.out.print("\nValor ");
-        for (int i = 0; i < r.length; i++) {
-            System.out.printf("%5d", r[i]);
-        }
-        sc.close();
+
+        System.out.print("*    *  ****  ****** *    *\n");
     }
 }
 
